9eaf96ea669300bfc6f8cdcad33b864d3c4a66e5
rchinhamhora <rchinhamhora@gmail.com>
2021-05-06 21:40:39 -0500

    comments added

--- src/main/java/dmacc/drama/entities/DecisionTree.java
+++ src/main/java/dmacc/drama/entities/DecisionTree.java
@@ -8,11 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * @author Rumbi
- * Date: 4/30/21
- * Time: 6:42 AM
- */
+/**************************************************************
+* Name        : Eastern Dramas
+* Author      : Rumbi Chinhamhora
+* Created     : 4/24/2021
+* Course      : CIS 152 Data Structures
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
 
 @Data
 public class DecisionTree {
